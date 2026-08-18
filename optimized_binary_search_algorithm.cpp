e2d094781b00259010d6fa094f095f243b008916
VISHWAJEET SINGH <vishwajeetsinghrajput75@gmail.com>
2026-08-18 19:12:41 +0530

    Implement optimized binary search algorithm

---
+++ optimized_binary_search_algorithm.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<climits>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main(){
+    int n,i,j;
+    cout<<"enter no. of elements: "<<endl;
+    cin>>n;
+    
+    vector<int> a(n);
+    
+    cout<<"enter elements: "<<endl;
+    for(i=0;i<n;i++){
+    cin>>a[i];
+    }
+    
+    cout<<"\narray:\n "<<endl;
+    for(i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    
+    sort(a.begin(),a.end());
+    
+    cout<<"\nsorted array:\n "<<endl;
+    for(i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    
+    int target;
+    cout<<"\nenter target: "<<endl;
+    cin>>target;
+    int start=0,end=n-1;
+    
+    while(start<=end){
+        int mid=start+((end-start)/2);
+        if(target>a[mid]){
+            start=mid+1;
+        }
+        else if(target<a[mid]){
+            end=mid-1;
+        }
+        else if(target==a[mid]){
+            cout<<"ans index: "<<mid;
+            return 0;
+        }
+    }
+    cout<<"\nans not found "<<endl;
+    return 0;
+}
